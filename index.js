6001a82f6635dc31a53d25b782c37c9fd96844d2
Maryam Tirmazi <maryamtirmazi@Maryams-MacBook-Pro.local>
2024-07-29 16:16:44 +0400

    changes

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-function search(){    
+document.getElementById("search").addEventListener("input", search);
+var text = document.getElementById("demo").innerHTML
+
+function search(){  
+    
+    text = ""
+
+    var input = this.value.toLowerCase();
+    const recipe_names = document.querySelectorAll("recipe_name");
+
+    recipe_names.forEach(function check(item){
+        var name = item.textcontent.toLowerCase()
+        if (name.includes(input)){
+
+            text += name;
+
+        }
+    })
+
 
 
 }
