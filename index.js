e7fc6b6dc39476f72787d7b47971d4da42377eee
Maryam Tirmazi <maryamtirmazi@Maryams-MacBook-Pro.local>
2024-07-29 15:04:35 +0400

    changes

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
-const recipe_names = []; 
-function search(){
-
-    var searchingFor = document.getElementById('search').innerHTML; 
-
-    for (i = 0; i < 6; i++){
-
-        recipe_names[i] = document.getElementById(i).innerHTML;
-
-    }
-
-    alert(recipe_names.toString());
-
-
-    //alert("You searched for : " + searchingFor);
+function search(){    
 
 
 }
